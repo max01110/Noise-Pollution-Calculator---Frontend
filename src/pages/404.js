61eb874dbd47d1832779088c6f17b299da6f9b5b
Maxime <michet.maxime@gmail.com>
2020-12-30 00:15:06 -0500

    update readme

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>404!</h1>
+            <small>Not Found</small><br/>
+            <small>Sorry...</small>
+        </div>
+    )
+}
+
+export default NotFoundPage
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>Index!</h1>
+            <small>Noise Pollution Calculator</small><br/>
+            <small>Yay</small>
+        </div>
+    )
+}
+
+export default NotFoundPage
